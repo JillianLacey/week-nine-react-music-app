2d155447fc35dbdfd24689775649f132a047aebc
Jillian Pichocki <jpichocki@gmail.com>
2017-09-15 15:33:44 -0500

    fully functional needs styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 
-export default class PlayList extends Component {
+export default class NavBar extends Component {
     render() {
         return (
             <div>
-
+                HELLLLLLLOOOO  JERRRY
             </div>
         )
     }
